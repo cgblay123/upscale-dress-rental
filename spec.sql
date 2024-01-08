1979bb75d5dfb287b6512bfb6e28f35f83dc0d76
cgblay123 <cgblaivas@gmail.com>
2024-01-08 18:55:19 -0500

    added spec

---
+++ spec.sql
@@ -0,0 +1,26 @@
+/*
+I have dress rental company called Upscale that rents nice dresses for close family/friend's events. I would like to have a database to store information about all of my dresses, including: how much I paid for each dress, date I bought it, the order number, retail value of the dress, the company of the dress, brief description of dress's style, the size, how much I charge to rent it, how many times it was rented, current profit per dress, if there was any damage to the dress and how much it cost to repair, sellout date (once a dress was rented many times I will try to sell it), and price it was sold for.
+
+Reports needed:
+1) all brand new dresses
+2) all dresses rented more than 7 times
+3) all dresses that the amount made from renting them out is still less than the price I paid
+4) all dresses that have been re-sold
+5) average amount below retail price that I pay for my dresses
+
+Questions:
+Question: What size range do you carry?
+Answer: 0-14
+
+Question: What date did you open?
+Answer: I opened on December 11, 2023, but I bought my first dress on Oct 10, 2023
+
+Question: Is there a minimum/maximum that you will pay for a dress?
+Answer: No, I might get a dress for free (it hasn't happened yet, but I am hopeful), and I'm not sure what inflation will bring...
+
+Sample Data:
+$200, 11/17/2023, n/a, $450, Sharrone, blue lace tiered maxi, 2, $100, 1, n/a, n/a, n/a
+$150, 1/8/2024, 4000875, $648, Knitted, beige eyelet midi, 2, $100, 0, n/a, n/a, n/a
+$222, 1/1/2024, n/a, $424, Swift Lines, emerald and black lace with belt, 0, $100, 3, belt ripped, $10, n/a, n/a
+$500, 10/10/2023, 100798548, Zimmerman, black silk with gold buttons, 6, $986, 6, n/a, n/a, n/a
+*/
