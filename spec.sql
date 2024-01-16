881457b99a8c49d6cb2c88d07a09c5f9c5d0f976
cgblay123 <61660229+cgblay123@users.noreply.github.com>
2024-01-15 19:52:56 -0500

    Update spec.sql

--- spec.sql
+++ spec.sql
@@ -1,12 +1,12 @@
 /*
-I have dress rental company called Upscale that rents nice dresses for close family/friend's events. I would like to have a database to store information about all of my dresses, including: how much I paid for each dress, date I bought it, the order number, retail value of the dress, the company of the dress, brief description of dress's style, the size, how much I charge to rent it, how many times it was rented, current profit per dress, if there was any damage to the dress and how much it cost to repair, sellout date (once a dress was rented many times I will try to sell it), and price it was sold for.
+I have dress rental company named 'Upscale' that rents nice dresses for close family/friend's events. I would like to have a database to store information about all of my dresses, including: how much I paid for each dress, date I bought it, the order number, retail value of the dress, the company of the dress, brief description of dress's style, the size, how much I charge to rent it, how many times it was rented, current profit per dress, if there was any damage to the dress and how much it cost to repair, sellout date (once a dress was rented many times I will try to sell it), and price it was sold for.
 
 Reports needed:
-1) all brand new dresses
-2) all dresses rented more than 7 times
-3) all dresses that the amount made from renting them out is still less than the price I paid
-4) all dresses that have been re-sold
-5) average amount below retail price that I pay for my dresses
+1) Show a list of all brand new dresses (that have never been rented yet).
+2) I plan to only sell dresses that have been rented at least 7 times. Please show all dresses that have been rented 7 times or more.
+3) List all dresses that the price I paid for them is still more than the amount I made renting it.
+4) List all dresses that have been re-sold.
+5) I would like to know what is the average amount below retail price that I pay for my dresses.
 
 Questions:
 Question: What size range do you carry?
@@ -18,7 +18,11 @@ Answer: I opened on December 11, 2023, but I bought my first dress on Oct 10, 20
 Question: Is there a minimum/maximum that you will pay for a dress?
 Answer: No, I might get a dress for free (it hasn't happened yet, but I am hopeful), and I'm not sure what inflation will bring...
 
+Question: How long after you bought a dress will you consider selling it?
+Answer: I don't have a specfic timeframe, it will be based on how many times the dress was rented and the condition it is in.
+
 Sample Data:
+Price paid, date purchased, order number, retail value, company, description, size, rental price, times rented, profit, was damaged, damage costs, sellout date
 $200, 11/17/2023, n/a, $450, Sharrone, blue lace tiered maxi, 2, $100, 1, n/a, n/a, n/a
 $150, 1/8/2024, 4000875, $648, Knitted, beige eyelet midi, 2, $100, 0, n/a, n/a, n/a
 $222, 1/1/2024, n/a, $424, Swift Lines, emerald and black lace with belt, 0, $100, 3, belt ripped, $10, n/a, n/a
